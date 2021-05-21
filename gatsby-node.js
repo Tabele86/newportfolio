1618c1ca5ee9e66e0ef1b936d35bf0316b4661ad
Tokur86 <tabele86@gmail.com>
2021-05-21 13:01:37 -0500

    field-type issue

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,33 @@
-const path = require(`path`);
+const path = require('path');
 
+const getFileNode = (options) => (source, _, context, info) => {
+	const { fieldName } = info;
+	const partialPath = source[fieldName];
+
+	if (!partialPath) {
+		return null;
+	}
+
+	const filePath = path.join(__dirname, options.path, partialPath);
+
+	const fileNode = context.nodeModel.runQuery({
+		firstOnly: true,
+		type: 'File',
+		query: {
+			filter: {
+				absolutePath: {
+					eq: filePath
+				}
+			}
+		}
+	});
+
+	if (!fileNode) {
+		return null;
+	}
+
+	return fileNode;
+};
 exports.createPages = async ({ graphql, actions }) => {
 	const { data } = await graphql(`
       query Articles {
@@ -20,4 +48,23 @@ exports.createPages = async ({ graphql, actions }) => {
 			context: { slug: node.frontmatter.slug }
 		});
 	});
+
+	createFieldExtension({
+		name: 'fileByAbsolutePath',
+		args: {
+			path: {
+				type: 'String!',
+				defaultValue: ''
+			}
+		},
+		extend: (options) => ({
+			resolve: getFileNode(options)
+		})
+	});
+
+	createTypes(`
+  type LocalContentCities implements Node {
+    image: File @fileByAbsolutePath(path: "src/images")
+  }
+`);
 };
